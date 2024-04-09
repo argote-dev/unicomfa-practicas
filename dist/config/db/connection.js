07a9e907c8d60a0be1483e1021bd1de840cba6fe
David Argote <argote.2075@gmail.com>
2024-04-09 18:29:59 -0500

    Se cambia js como lenguage común

--- dist/config/db/connection.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.connection = void 0;
-const client_1 = require("@prisma/client");
-exports.connection = new client_1.PrismaClient();
--- src/config/routes/index.ts
+++
@@ -1,6 +0,0 @@
-export const routers = (app: { get: (arg0: string, arg1: any) => void; use: (arg0: string, arg1: any) => void }) => {
-  app.get('/', async (_req: any, res: { send: (arg0: string) => void }) => {
-    res.send('Application [Practicas] Online');
-  });
-  app.use('/api/auth/', require('../../components/auth/infrastructure/controller'));
-};
