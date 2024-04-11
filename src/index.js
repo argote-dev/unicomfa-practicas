333121951ffe8cfb22a3543ac2e0fb26f0d3716b
David Argote <argote.2075@gmail.com>
2024-04-11 18:57:42 -0500

    Add swagger docs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 const app = require('./config/application');
-const swaggerDocs = require('./swagger');
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
 
 const port = app.get('port');
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+
+app.use('/api/v1/users', require('./components/user/infrastructure/routes/user.routes'));
 
 app.on('error', (error) => {
   console.log(`❌ ${error}`);
@@ -9,5 +14,4 @@ app.on('error', (error) => {
 
 app.listen(port, () => {
   console.log(`✅ Server is running at http://localhost:${port}`);
-  swaggerDocs(app);
 });
