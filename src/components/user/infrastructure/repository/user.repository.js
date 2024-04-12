653c214da0b55a27bee8ba87ab55ccb14ab11c7d
David Argote <argote.2075@gmail.com>
2024-04-11 20:46:33 -0500

    Agregado nuevas tablas en el esquema

--- src/components/user/infrastructure/repository/user.repository.js
+++
@@ -1,6 +0,0 @@
-module.exports = class UserRepository {
-  insert = async (req, res) => {};
-  list = async (req, res) => {};
-  update = async (req, res) => {};
-  delet = async (req, res) => {};
-};
