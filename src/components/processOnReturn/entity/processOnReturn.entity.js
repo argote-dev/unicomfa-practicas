1e9d14017a4fa0e2a1dc2a3e89d9d76ef3df509a
ArcosGomez <162403364+ArcosGomez@users.noreply.github.com>
2024-04-21 10:25:16 -0500

    Update processOnReturn.entity.js

--- src/components/processOnReturn/entity/processOnReturn.entity.js
+++ src/components/processOnReturn/entity/processOnReturn.entity.js
@@ -1,6 +1,6 @@
 module.exports = class processOnReturn {
-    constructor(processId, returnId) {
-        this.processId = processId;
-        this.returnId = returnId;
+    constructor(process_Id, return_Id) {
+        this.process_Id = process_Id;
+        this.return_Id = return_Id;
     }
 };
