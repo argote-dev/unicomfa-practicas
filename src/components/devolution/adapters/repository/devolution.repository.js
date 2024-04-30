0e758a0de85d0628ccafa86b44d4c666944442e1
Nicolas Arcos <nicolasarcos@unicomfacauca.edu.com>
2024-04-30 18:44:34 -0500

    Se modifico el nombre de la clase Respositorio

--- src/components/devolution/adapters/repository/devolution.repository.js
+++ src/components/devolution/adapters/repository/devolution.repository.js
@@ -2,7 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 
 const prisma = new PrismaClient();
 
-module.exports = class UserRepository {
+module.exports = class DevolutionRepository {
     insert = async (devolution) => {
         await prisma.devolution.create({ data: devolution });
     };
