d929fea17b46debe9310f8644925aca88a9b6730
ArcosGomez <nicolasarcos@unicomfacauca.edu.co>
2024-05-29 11:47:57 -0500

    Se agrego la rama TypeDepartment con sus elementos y se agrego en el index y swager

---
+++ src/components/typeDepartment/adapters/controller/typeDepartment.controller.js
@@ -0,0 +1,53 @@
+module.exports = class typeDepartmentController {
+    constructor(createTypeDepartment, lisTypeDepartment, updateTypeDepartment, deleteTypeDepartment) {
+      this.createTypeDepartment = createTypeDepartment;
+      this.lisTypeDepartment = lisTypeDepartment;
+      this.updateTypeDepartment = updateTypeDepartment;
+      this.deleteTypeDepartment = deleteTypeDepartment;
+    }
+  
+    async insertTypeDepartment(req, res) {
+      const { name,} = req.body;
+      try {
+        await this.createProgram.execute({
+          name,
+        });
+        res.status(201).send({});
+      } catch {
+        res.status(500).send({ message: 'An error ocurred while creating the typeDepartment.' });
+      }
+    }
+  
+    async listAllTypeDepartment(req, res) {
+      try {
+        let data = await this.lisTypeDepartment.execute();
+        res.status(200).send(data);
+      } catch {
+        res.status(500).send({ message: 'An error ocurred while list the typeDepartament.' });
+      }
+    }
+  
+    async deleteTypeDepartmentById(req, res) {
+      try {
+        const id = req.params.id;
+        await this.deleteTypeDepartment.execute(Number(id));
+        res.status(200).send(data);
+      } catch {
+        res.status(500).send({ message: 'An error ocurred while delete the typeDepartment.' });
+      }
+    }
+  
+    async updateTypeDepartmentInfo(req, res) {
+      const { name } = req.body;
+      try {
+        await this.updateTypeDepartment.execute({
+          name,
+          
+        });
+        res.status(200).send(data);
+      } catch {
+        res.status(500).send({ message: 'An error ocurred while update the typeDepartment.' });
+      }
+    }
+  };
+  
---
+++ src/components/typeDepartment/adapters/routes/typeDepartment.routes.js
@@ -0,0 +1,130 @@
+const express = require('express');
+const router = express.Router();
+
+const TypeDepartmentRepository = require('../repository/typeDepartment.repository');
+const CreateTypeDepartmentUseCase = require('../../usecase/create.usecase');
+const ListTypeDepartmentUseCase = require('../../usecase/list.usecase');
+const UpdateTypeDepartmentUseCase = require('../../usecase/update.usecase');
+const DeleteTypeDepartmentUsecase = require('../../usecase/delete.usecase');
+const TypeDepartmentController = require('../controller/typeDepartment.controller');
+
+const typeDepartmentRepository = new TypeDepartmentRepository();
+const createTypeDepartmentUseCase = new CreateTypeDepartmentUseCase(typeDepartmentRepository);
+const listTypeDepartmentUseCase = new ListTypeDepartmentUseCase(typeDepartmentRepository);
+const updateTypeDepartmentUseCase = new UpdateTypeDepartmentUseCase(typeDepartmentRepository);
+const deleteTypeDepartmentUseCase = new DeleteTypeDepartmentUsecase(typeDepartmentRepository);
+const typeDepartmentController = new TypeDepartmentController(createTypeDepartmentUseCase, listTypeDepartmentUseCase, 
+    updateTypeDepartmentUseCase, deleteTypeDepartmentUseCase);
+
+/**
+ * @swagger
+ * /api/v1/typeDepartment:
+ *   post:
+ *     tags:
+ *      - TypeDepartment
+ *     summary: Insert new typeDepartment
+ *     description: Save typeDepartment into db.
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties:
+ *              name:
+ *                type: string
+ *                example: Popayán
+ *     responses:
+ *          '201':
+ *             description: typeDepartment created
+ *          '500':
+ *             description: Server error
+ */
+router.post('/', async (req, res) => {
+  await typeDepartmentController.insertTypeDepartment(req, res);
+});
+
+/**
+ * @swagger
+ * /api/v1/typeDepartment:
+ *   get:
+ *     tags:
+ *       - TypeDepartment
+ *     summary: Get all TypeDepartment registered in the database
+ *     description: List of all typeDepartment.
+ *     responses:
+ *       '200':
+ *         description: TypeDepartment found
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 id:
+ *                   type: integer
+ *                   format: int64
+ *                   example: 1
+ *                 name:
+ *                   type: string
+ *                   example: Popayán
+ */
+router.get('/', async (req, res) => {
+  await typeDepartmentController.listAllTypeDepartment(req, res);
+});
+
+/**
+ * @swagger
+ * /api/v1/typeDepartment:
+ *   put:
+ *     tags:
+ *      - TypeDepartment
+ *     summary: Update typeDepartment
+ *     description: Update info of typeDepartment.
+ *     requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            type: object
+ *            properties: 
+ *              name:
+ *                type: string
+ *                example: Popayán
+ *     responses:
+ *          '200':
+ *             description: TypeDepartment created
+ *          '500':
+ *             description: Server error
+ */
+router.put('/', async (req, res) => {
+  await typeDepartmentController.updateTypeDepartmentInfo(req, res);
+});
+
+/**
+ * @swagger
+ * /api/v1/typeDepartment/{id}:
+ *   delete:
+ *     tags:
+ *       - TypeDepartment
+ *     summary: Delete typeDepartment by id
+ *     description: Delete register of typeDepartment.
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: id of typeDepartment
+ *         schema:
+ *           type: string
+ *     responses:
+ *       '200':
+ *         description: TypeDepartment deleted
+ *       '404':
+ *         description: TypeDepartment not found
+ *       '500':
+ *         description: Server internal error
+ */
+router.delete('/:id', async (req, res) => {
+  await typeDepartmentController.deleteTypeDepartmentById(req, res);
+});
+
+module.exports = router;
